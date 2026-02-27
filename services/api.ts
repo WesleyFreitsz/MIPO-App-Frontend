e6a6bffc3d8897cd3abac05879cdbfe97bba708a
Wesley Vinicius <wesleyviniciusfreitas.jr@gmail.com>
2026-02-27 16:32:06 -0300

    atualizando url

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "http://192.168.3.61:3000", // Substitua pelo IP do seu backend
+  baseURL: "https://mipo-app-backend-production.up.railway.app", // URL atualizada
 });
 // Interceptor: Adiciona o Token automaticamente em toda requisição
 api.interceptors.request.use(async (config) => {
